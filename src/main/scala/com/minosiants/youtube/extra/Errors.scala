3deff752d000a276fa64c694b90adbc56dc4bbf1
Kaspar Minosiants <kaspar.ru@gmail.com>
2020-02-08 18:32:29 +0300

    Update FileUtils to use Resource

--- src/main/scala/com/minosiants/youtube/extra/Errors.scala
+++ src/main/scala/com/minosiants/youtube/extra/Errors.scala
@@ -2,6 +2,7 @@ package com.minosiants
 package youtube.extra
 
 import cats.Show
+import cats.effect.IO
 
 import scala.util.control.ControlThrowable
 
@@ -10,6 +11,10 @@ sealed abstract class Error extends ControlThrowable
 final case class CommandNotFound(message: String)     extends Error
 final case class NoRequiredArguments(message: String) extends Error
 final case class PlaylistNotFound(message: String)    extends Error
+final case class ResourceNotFound(message: String)    extends Error
+final case class UnableCreateDir(message: String)     extends Error
+final case class UnableWriteFile(message: String)     extends Error
+final case class UnableCloseResource(message: String) extends Error
 
 object Error {
 
@@ -17,5 +22,18 @@ object Error {
     case CommandNotFound(message)     => s"Command not found: $message"
     case NoRequiredArguments(message) => s"No required arguments: $message"
     case PlaylistNotFound(message)    => message
+    case ResourceNotFound(message)    => s"Resource not found: $message"
+    case UnableCreateDir(message)     => s"Unable to create directory: $message"
+    case UnableWriteFile(message)     => s"Unable write to file: $message"
+    case UnableCloseResource(message) => s"Unable close resource: $message"
   }
+
+  def unableWriteFile[A](message: String): IO[A] =
+    IO.raiseError(UnableWriteFile(message))
+
+  def unableCloseResource[A](message: String): IO[A] =
+    IO.raiseError(UnableCloseResource(message))
+
+  def unableCreateDir[A](message: String): IO[A] =
+    IO.raiseError(UnableCreateDir(message))
 }
--- src/main/scala/com/minosiants/youtube/extra/FileUtils.scala
+++ src/main/scala/com/minosiants/youtube/extra/FileUtils.scala
@@ -2,34 +2,46 @@ package com.minosiants
 package youtube.extra
 
 import java.io.{ File, InputStream, PrintWriter }
-import java.net.URL
 
-import cats.effect.IO
-
-import scala.util.Try
+import cats.effect.{ IO, Resource }
 
 object FileUtils {
 
   def saveToFile(text: String, destination: File): IO[Unit] =
-    IO.fromTry(Try[Unit] {
-      val p = new PrintWriter(destination)
-      p.write(text)
-      p.close()
-    })
+    printWriter(destination)
+      .use(pw => IO(pw.write(text)))
+      .handleErrorWith(_ => Error.unableWriteFile(destination.getAbsolutePath))
 
-  def loadFile(file: String): IO[String] =
-    IO.fromTry(Try {
-      val is     = getClass().getResourceAsStream(file)
-      val result = scala.io.Source.fromInputStream(is).mkString
-      is.close()
-      result
-    })
+  def printWriter(file: File): Resource[IO, PrintWriter] =
+    Resource.make {
+      IO(new PrintWriter(file))
+    } { pw =>
+      IO(pw.close())
+        .handleErrorWith(_ => Error.unableCloseResource(file.getAbsolutePath))
+    }
+
+  def inputStream(file: String): Resource[IO, InputStream] =
+    Resource.make {
+      IO(getClass().getResourceAsStream(file))
+    } { is =>
+      IO(is.close()).handleErrorWith(_ => Error.unableCloseResource(file))
+    }
+
+  def loadFile(file: String): IO[String] = inputStream(file).use { is =>
+    IO {
+      scala.io.Source.fromInputStream(is)
+    }.bracket(bs => IO(bs.mkString)) { bss =>
+      bss.close()
+      IO.unit
+    }
+  }
 
   def mkParentDirs(file: File): IO[Unit] =
-    IO.fromTry(Try[Unit] {
-      val parent = file.getParentFile()
-      if (!parent.exists() && !parent.mkdirs()) {
-        throw new IllegalStateException("Couldn't create dir: " + parent);
-      }
-    })
+    IO(file.getParentFile()).bracket { parent =>
+      if (!parent.exists() && !parent.mkdirs())
+        Error.unableCreateDir(parent.getAbsolutePath)
+      else
+        IO.unit
+    }(_ => IO.unit)
+
 }
