9cef7977ac07ac0fd07e818d5d9eb9da177946c7
Kaspar Minosiants <kaspar.ru@gmail.com>
2020-02-11 21:12:42 +0300

    Refactor

--- src/main/scala/com/minosiants/youtube/extra/YoutubeExtraApp.scala
+++ src/main/scala/com/minosiants/youtube/extra/YoutubeExtraApp.scala
@@ -5,8 +5,9 @@ import java.io.File
 
 import cats.effect.{ ContextShift, ExitCode, IO }
 import cats.instances.string._
-import org.http4s.client.blaze.BlazeClientBuilder
+import cats.syntax.functor._
 
+import org.http4s.client.blaze.BlazeClientBuilder
 import scala.concurrent.ExecutionContext
 import PlaylistGenerator.createPlaylist
 import YoutubeDataAccessProps.props
@@ -29,14 +30,14 @@ case class YoutubeExtraApp()(
       playlistId: String,
       token: String,
       destination: File
-  ): IO[Unit] = withClient[Unit](token) { client =>
+  ): IO[Unit] = withClient(token) { client =>
     for {
       playlist <- client.getFullPlaylist(playlistId)
       _        <- createPlaylist(playlist, destination)
     } yield ()
   }
 
-  def help(): IO[Unit] = IO {
+  def help(): IO[Unit] = {
     val message =
       """
         |youtube-extra: usage
@@ -51,7 +52,6 @@ case class YoutubeExtraApp()(
         |""".stripMargin
 
     c.putStrLn(message)
-
   }
 
   def runApp(args: List[String]): IO[ExitCode] =
@@ -69,10 +69,8 @@ case class YoutubeExtraApp()(
       .flatMap {
         case Right(_) => IO(ExitCode.Success)
         case Left(e: Error) =>
-          c.putStrLn(e)
-          IO(ExitCode.Error)
+          c.putStrLn(e).as(ExitCode.Error)
         case Left(error) =>
-          c.putStrLn(error.getMessage)
-          IO(ExitCode.Error)
+          c.putStrLn(error.getMessage).as(ExitCode.Error)
       }
 }
